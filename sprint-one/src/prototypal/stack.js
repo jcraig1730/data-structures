068af9f4f2a83693003af2164077198d2721c493
Jared Craig <jcraig1730@gmail.com>
2019-07-17 22:03:42 -0700

    Complete prototypal stack.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.stackSize = 0;
+  return someInstance;
 };
 
+// console.log('booya');
 var stackMethods = {};
+stackMethods.push = function(value) {
+  if (!this.stackSize) {
+    this[0] = value;
+    this.stackSize++;
+  } else {
+    this[this.stackSize] = value;
+    this.stackSize++;
+  }
+};
 
+stackMethods.pop = function() {
+  var target = this[this.stackSize - 1];
+  delete this[this.stackSize - 1];
+  this.stackSize--;
+  if (this.stackSize < 0) this.stackSize = 0;
+  return target;
+};
 
+stackMethods.size = function() {
+  return this.stackSize;
+};
